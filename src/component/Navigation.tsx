65999144ef90226581615b0c4a94dba9d7b1c357
Suisuikitai <mag.cupw@gmail.com>
2021-07-04 02:54:03 +0900

    add Navigation

---
+++ src/component/Navigation.tsx
@@ -0,0 +1,33 @@
+export default function Navigation() {
+  return (
+    <div>
+      <div>
+        <a href='#'>
+          <img src='#' alt='logo' />
+        </a>
+      </div>
+      <div>
+        <div>
+          <a href=''>Features</a>
+        </div>
+        <div>
+          <a href=''>Pricing</a>
+        </div>
+        <div>
+          <a href=''>Community</a>
+        </div>
+        <div>
+          <a href=''>Support</a>
+        </div>
+      </div>
+      <div>
+        <div>
+          <a href=''>Log in</a>
+        </div>
+        <div>
+          <a href=''>Register</a>
+        </div>
+      </div>
+    </div>
+  )
+}
